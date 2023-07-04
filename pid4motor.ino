a415c1b49f59cc6c6a56f8e411420f324c6fca1e
YoungChill <crt4pub@gmail.com>
2023-07-04 16:39:49 +0800

    通过按键选择调试，去除多余注释

--- pid4motor.ino
+++ pid4motor.ino
@@ -5,67 +5,63 @@
 /*读取两相编码*/
 #define motorA 9
 #define motorB 10
+/*电机引脚*/
+#define Kp 260 // 瞬态响应
+#define Ki 2.0  // 稳态误差、精准度
+#define Kd 2000  // 震荡
 
-/*In this section we have defined the gain values for the 
- * proportional, integral, and derivative controller I have set
- * the gain values with the help of trial and error methods.
-*/ 
-#define Kp 260 // Proportional constant
-#define Ki 2.7  // Integral Constant
-#define Kd 2000  // Derivative Constant
+volatile long int encodeCount = 0; // 电机编码数，判断位置、方向
+unsigned int aimPosition = 0; // 调试目的值
+char incomingByte; // 串口位解析
+int motorPwmValue = 255; // 最后控制输出的PID值
+int chosenPositionNum=1;
 
-volatile long int encodeCount = 0; // stores the current encoder count
-unsigned int integerValue = 0; // stores the incoming serial value. Max value is 65535
-char incomingByte; // parses and stores each character one by one
-int motorPwmValue = 255; // after PID computation data is stored in this variable.
 PIDController pidcontroller;
 
 void setup() {
-  Serial.begin(9600); // Serial for Debugging
-  pinMode(encodeA, INPUT); // encodeA as Input
-  pinMode(encodeB, INPUT); // encodeB as Input
-  pinMode(motorA, OUTPUT); // motorA as Output
-  pinMode(motorB, OUTPUT); // motorA as Output
-/* attach an interrupt to pin encodeA of the Arduino, and when the pulse is in the RISING edge called the function encoder().
-*/
+  Serial.begin(9600); 
+  pinMode(encodeA, INPUT); 
+  pinMode(encodeB, INPUT); 
+  pinMode(motorA, OUTPUT); 
+  pinMode(motorB, OUTPUT); 
+
   attachInterrupt(digitalPinToInterrupt(encodeA), encoder, RISING);
-  pidcontroller.begin(); // initialize the PID instance
-  pidcontroller.tune(Kp , Ki , Kd); // Tune the PID, arguments: kP, kI, kD
-  pidcontroller.limit(-255, 255); // Limit the PID output between -255 to 255, this is important to get rid of integral windup!
+//  attachInterrupt(1, getCurrentPosition, RISING);
+  pidcontroller.begin(); // 初始化PID控制器
+  pidcontroller.tune(Kp , Ki , Kd); //设置参数
+  pidcontroller.limit(-255, 255); //设置PID输出范围
 }
 
 void loop() {
   while (Serial.available() > 0) {
-    integerValue = Serial.parseInt(); // stores the integerValue
-    incomingByte = Serial.read(); // stores the /n character
-    pidcontroller.setpoint(integerValue); // The "goal" the PID controller tries to "reach",
-    Serial.println(integerValue); // print the incoming value for debugging
-    if (incomingByte == '\n') // if we receive a newline character we will continue in the loop
-      continue;
+    chosenPositionNum = Serial.parseInt(); // 目标按键
+    choosePosition(chosenPositionNum);
+    Serial.read(); // 读取换位符
+    pidcontroller.setpoint(aimPosition); // 目标值
   }
-  motorPwmValue = pidcontroller.compute(encodeCount);  //Let the PID compute the value, returns the calculated optimal output
-  Serial.print(motorPwmValue); // print the calculated value for debugging
+  motorPwmValue = pidcontroller.compute(encodeCount);  //PID计算所需的值
+  Serial.print(motorPwmValue); // PWN值
   Serial.print("   ");
-  if (motorPwmValue > 0) // if the motorPwmValue is greater than zero we rotate the  motor in clockwise direction
+  if (motorPwmValue > 0) //逆时针
     motorReserve(motorPwmValue);
-  else // else, we move it in a counter-clockwise direction
+  else // 顺时针
     motorForward(abs(motorPwmValue));
-  Serial.println(encodeCount);// print the final encoder count.
+  Serial.println(chosenPositionNum); // 打印当前选择的位置
 }
 
 void encoder() {
-  if (digitalRead(encodeB) == HIGH) // if encodeB is high increase the count
-    encodeCount++; // increment the count
-  else // else decrease the count
-    encodeCount--; // decrement the count
+  if (digitalRead(encodeB) == HIGH) 
+    encodeCount++; 
+  else 
+    encodeCount--; 
 }
 
 void motorForward(int power) {
-  if (power > 100) {
+  if (power > 50) {
     analogWrite(motorA, power);
     digitalWrite(motorB, LOW);
   }
-// both of the pins are set to low
+
   else {
     digitalWrite(motorA, LOW);
     digitalWrite(motorB, LOW);
@@ -73,7 +69,7 @@ void motorForward(int power) {
 }
 
 void motorReserve(int power) {
-  if (power > 100) {
+  if (power > 50) {
     analogWrite(motorB, power);
     digitalWrite(motorA, LOW);
   }
@@ -81,4 +77,32 @@ void motorReserve(int power) {
     digitalWrite(motorA, LOW);
     digitalWrite(motorB, LOW);
   }
+}
+void choosePosition(int chosenPosition){
+  switch(chosenPosition){
+    case 1:
+      aimPosition=0;
+      break;
+    case 2:
+      aimPosition=63;
+      break;
+    case 3:
+      aimPosition=126;
+      break;
+    case 4:
+      aimPosition=189;
+      break;
+    case 5:
+      aimPosition=252;
+      break;
+    case 6:
+      aimPosition=315;
+      break;
+    default:
+      aimPosition=0;
+  }
+}
+
+void getCurrentPosition(){
+  
 }
