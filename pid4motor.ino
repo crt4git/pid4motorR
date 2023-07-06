9c7dcc54b252eea86f9da44fd32262427599b632
YoungChill <crt4pub@gmail.com>
2023-07-06 15:57:36 +0800

    集成pid

--- pid4motor.ino
+++ pid4motor.ino
@@ -1,25 +1,31 @@
-#include <PIDController.h>
-PIDController pidcontroller;
-
+/*读取两相编码*/
 #define encodeA 2 
 #define encodeB 5
-/*读取两相编码*/
+/*电机引脚*/
 #define motorA 9
 #define motorB 10
-/*电机引脚*/
-#define A0_pin 23 //编码0
-#define A1_pin 24 //编码1
-#define A2_pin 11 //编码2
 /*霍尔传感器*/
-#define Kp 260 // 瞬态响应
-#define Ki 2.0  // 稳态误差、精准度
-#define Kd 2000  // 震荡
+#define A0Pin 23 //编码0
+#define A1Pin 24 //编码1
+#define A2Pin 11 //编码2
         
-volatile long int encodeCount = 0; // 电机编码数，判断位置、方向
-unsigned int aimPosition=0;; // 目的值
+volatile double nowPosition = 0; // 电机编码数，判断位置、方向
+double aimPosition=0;; // 目的值
 int motorPwmValue = 255; // 最后控制输出的PID值
-int chosenPositionNum=1; //通道选择
+int chosenChannelNum=1; //通道选择
 bool initialized=false; //初始化完成标志
+
+/*pid控制器参数*/
+double Kp=260;// 瞬态响应
+double Ki=2.0;// 稳态误差、精准度
+double Kd=2000;// 震荡
+double divisor=10;
+double minOut=-255;
+double maxOut=255;
+double output=0;
+double lastErr=0;
+double errSum=0;
+unsigned long lastTime=0;
 
 
 void setup() {
@@ -31,36 +37,32 @@ void setup() {
 
   attachInterrupt(digitalPinToInterrupt(encodeA), encoder, RISING);
   attachInterrupt(1, motorInit, RISING);
-  pidcontroller.begin(); // 初始化PID控制器
-  pidcontroller.tune(Kp , Ki , Kd); //设置参数
-  pidcontroller.limit(-255, 255); //设置PID输出范围
 }
 
 void loop() {
   while (Serial.available() > 0) {
-    chosenPositionNum = Serial.parseInt(); // 目标按键
+    chosenChannelNum = Serial.parseInt(); // 目标按键
     Serial.read(); // 读取换位符
   }
-  choosePosition(chosenPositionNum);
-  pidcontroller.setpoint(aimPosition); // 目标值
-  motorPwmValue = pidcontroller.compute(encodeCount); //PID计算所需的值
+  chooseChannel(chosenChannelNum);
+  motorPwmValue=PIDController(aimPosition, nowPosition);
   if (motorPwmValue > 0) //逆时针
     motorReserve(motorPwmValue);
   else // 顺时针
     motorForward(abs(motorPwmValue));
-  Serial.println(chosenPositionNum); // 打印当前选择的位置
+  Serial.println(chosenChannelNum); // 打印当前选择的位置
 }
 
 void encoder() {
   if (digitalRead(encodeB) == HIGH) 
-    encodeCount++; 
+    nowPosition++; 
   else 
-    encodeCount--; 
+    nowPosition--; 
 }
 
-void motorForward(int power) {
-  if (power > 50) {
-    analogWrite(motorA, power);
+void motorForward(int inputPwmValue) {
+  if (inputPwmValue > 50) { //消除震荡
+    analogWrite(motorA, inputPwmValue);
     digitalWrite(motorB, LOW);
   }
   else {
@@ -68,9 +70,9 @@ void motorForward(int power) {
     digitalWrite(motorB, LOW);
   }
 }
-void motorReserve(int power) {
-  if (power > 50) {
-    analogWrite(motorB, power);
+void motorReserve(int inputPwmValue) {
+  if (inputPwmValue > 50) {
+    analogWrite(motorB, inputPwmValue);
     digitalWrite(motorA, LOW);
   }
   else {
@@ -78,40 +80,41 @@ void motorReserve(int power) {
     digitalWrite(motorB, LOW);
   }
 }
+
 /*光电获取初始化*/
 void motorInit(){
   if(initialized==false){
     getPosition();
-    aimPosition=encodeCount;
+    aimPosition=nowPosition;
     initialized=true;
   }
 }
 
 void getPosition(){
-  if(digitalRead(A0_pin)==1 && digitalRead(A1_pin)==1 &&digitalRead(A2_pin)==0){
-    encodeCount=0;
+  if(digitalRead(A0Pin)==1 && digitalRead(A1Pin)==1 &&digitalRead(A2Pin)==0){
+    nowPosition=0;
   }
-  else if(digitalRead(A0_pin)==1 && digitalRead(A1_pin)==0 &&digitalRead(A2_pin)==1){
-    encodeCount=63;
+  else if(digitalRead(A0Pin)==1 && digitalRead(A1Pin)==0 &&digitalRead(A2Pin)==1){
+    nowPosition=63;
   }
-  else if(digitalRead(A0_pin)==0 && digitalRead(A1_pin)==1 &&digitalRead(A2_pin)==1){
-    encodeCount=126;
+  else if(digitalRead(A0Pin)==0 && digitalRead(A1Pin)==1 &&digitalRead(A2Pin)==1){
+    nowPosition=126;
   }
-  else if(digitalRead(A0_pin)==0 && digitalRead(A1_pin)==1 &&digitalRead(A2_pin)==0){
-    encodeCount=189;
+  else if(digitalRead(A0Pin)==0 && digitalRead(A1Pin)==1 &&digitalRead(A2Pin)==0){
+    nowPosition=189;
   }
-  if(digitalRead(A0_pin)==1 && digitalRead(A1_pin)==0 &&digitalRead(A2_pin)==0){
-    encodeCount=252;
+  if(digitalRead(A0Pin)==1 && digitalRead(A1Pin)==0 &&digitalRead(A2Pin)==0){
+    nowPosition=252;
   }
-  else if(digitalRead(A0_pin)==0 && digitalRead(A1_pin)==0 &&digitalRead(A2_pin)==1){
-    encodeCount=315;
+  else if(digitalRead(A0Pin)==0 && digitalRead(A1Pin)==0 &&digitalRead(A2Pin)==1){
+    nowPosition=315;
   }
 }
 
 
 
-void choosePosition(int chosenPosition){
-  switch(chosenPosition){
+void chooseChannel(int inputChannel){
+  switch(inputChannel){
     case 1:
       aimPosition=0;
       break;
@@ -133,4 +136,25 @@ void choosePosition(int chosenPosition){
     default:
       aimPosition=0;
   }
+}
+
+/*pid控制器*/
+double PIDController(double aimPoint, double nowPoint){
+  //计算时间差
+  unsigned long now=millis();
+  double timeChange=(double)(now-lastTime);
+  
+  double error=aimPoint-nowPoint; //误差
+  errSum+=error*timeChange; //累积误差
+  //限制累积误差
+  errSum=constrain(errSum, minOut*1.1, maxOut*1.1); 
+  double dErr=(error-lastErr)/timeChange; //误差变化率
+
+  double newOutput=(Kp*error+Ki*errSum+Kd*dErr)/divisor;
+  output=constrain(newOutput, minOut, maxOut);
+//  output=newOutput;
+
+  lastErr=error;
+  lastTime=now;
+  return output;
 }
