d31a5dda52ef5f6ff6f322d853e4184c197562ae
YoungChill <crt4pub@gmail.com>
2023-07-17 14:09:39 +0800

    修改好引脚，基于光电初始化版本

--- pid4motor.ino
+++ pid4motor.ino
@@ -1,14 +1,14 @@
 #include <PIDController.h>
 
 #define encodeA 2 
-#define encodeB 5
+#define encodeB 3
 /*读取两相编码*/
-#define motorA 9
+#define motorA 11
 #define motorB 10
 /*电机引脚*/
-#define A0_pin 23;//编码0
-#define A1_pin 24;//编码1
-#define A2_pin 5;//编码2
+#define A0_pin 9;//编码0
+#define A1_pin 8;//编码1
+#define A2_pin 6;//编码2
 /*霍尔传感器*/
 
 #define Kp 260 // 瞬态响应
@@ -34,7 +34,7 @@ void setup() {
   pinMode(motorB, OUTPUT); 
 
   attachInterrupt(digitalPinToInterrupt(encodeA), encoder, RISING);
-  attachInterrupt(1, getCurrentPosition, RISING);
+//  attachInterrupt(1, getCurrentPosition, RISING);
   pidcontroller.begin(); // 初始化PID控制器
   pidcontroller.tune(Kp , Ki , Kd); //设置参数
   pidcontroller.limit(-255, 255); //设置PID输出范围
@@ -43,6 +43,9 @@ void setup() {
 }
 
 void loop() {
+  Serial.print(encodeCount    );
+  Serial.print(aimPosition    );
+  Serial.println(motorPwmValue);
   while (Serial.available() > 0) {
     chosenPositionNum = Serial.parseInt(); // 目标按键
     choosePosition(chosenPositionNum);
@@ -50,11 +53,11 @@ void loop() {
   }
   pidcontroller.setpoint(aimPosition); // 目标值
   motorPwmValue = pidcontroller.compute(encodeCount); //PID计算所需的值
-  if (motorPwmValue > 0) //逆时针
+  if (motorPwmValue > 0) //顺时针
     motorReserve(motorPwmValue);
-  else // 顺时针
+  else //逆时针
     motorForward(abs(motorPwmValue));
-  Serial.println(chosenPositionNum); // 打印当前选择的位置
+//  Serial.println(chosenPositionNum); // 打印当前选择的位置
 }
 
 void encoder() {
@@ -65,7 +68,7 @@ void encoder() {
 }
 
 void motorForward(int power) {
-  if (power > 50) {
+  if (power > 100) {
     analogWrite(motorA, power);
     digitalWrite(motorB, LOW);
   }
@@ -75,7 +78,7 @@ void motorForward(int power) {
   }
 }
 void motorReserve(int power) {
-  if (power > 50) {
+  if (power > 100) {
     analogWrite(motorB, power);
     digitalWrite(motorA, LOW);
   }
